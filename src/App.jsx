0465f069aa1a25ed9a60eb3a6eb4787835a592f6
shaonmh <shaonshan41@gmail.com>
2025-04-15 11:24:33 +0600

    next day commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,17 @@ function App() {
 
   return (
     <>
-      <div className="w-4/5 mx-auto flex justify-between ">
+      <div className="w-4/5 mx-auto flex justify-between items-center h-[100px]">
         <div className="logo">
-          <img src="src/assets/logo.png" alt="logo" />
+          <img src="src/assets/logo.png" alt="logo" className="h-[60px] w-auto" />
         </div>
         <div className="navbar h-100px ">
-          <ul className='flex gap-3 items-center mx-auto'>
+          <ul className='flex gap-8 items-center capitalize text-gray-500 '>
             <li><a href="#">home</a></li>
             <li><a href="#">Fixture</a></li>
             <li><a href="#">Teams</a></li>
             <li><a href="#">Schedules</a></li>
+            <li><a href="#" className='btn bg-transparent border text-black border-gray-500 px-4 py-2 rounded'>0 Coin</a></li>
           </ul>
         </div>
       </div>
