dad8d5eaa3165509de49066225e28395f65cfefb
shaonshan41 <shaonshan41@gmail.com>
2025-04-20 02:55:20 +0600

    added footer and copyright, added style

--- src/App.jsx
+++ src/App.jsx
@@ -87,12 +87,49 @@ function App() {
         </div>
       </div>
 
-      <div className="news-letter-container w-5/6 rounded-3xl flex flex-col mx-auto text-center items-center p-20 inset-0 bg-cover bg-center" style={{ backgroundImage: "url('/src/assets/bg-shadow.png')" }}>
+      <div className="news-letter-container  w-5/6 rounded-3xl flex flex-col mx-auto text-center items-center p-20 inset-0 bg-cover bg-center" style={{ backgroundImage: "url('/src/assets/bg-shadow.png')" }}>
         <h2 className='text-3xl  my-5 font-bold'>Subscribe to our Newsletter</h2>
         <h3 className='text-lg font-semibold text-gray-500'>Get the latest updates and news right in your inbox!</h3>
         <div className="sub flex items-center w-1/2 gap-3 py-5 justify-around ">
           <input type="text" className='h-14 appearance-none border border-slate-300 rounded-xl w-full px-10  py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Enter your email'/>
           <a href="#" className=' border-1 border-slate-300 bg-slate-300 px-6 py-4 font-bold rounded-xl h-14 bg-linear-65 from-pink-300 to-yellow-400'>Subscribe</a>
+        </div>
+      </div>
+
+      <div className="footer p-20 pb-0 text-gray-500   bg-slate-950 mx-auto  ">
+        <div className="logo my-20 flex justify-center">
+          <img src="/src/assets/logo-footer.png" alt="logo footer" />
+        </div>
+        <div className="footer-content grid grid-cols-3 gap-20 my-30">
+          <div className="about-us">
+            <h3>About US</h3>
+            <p>We are a passionate team dedicated to providing the best services to our customers.</p>
+          </div>
+          <div className="quicklinks">
+            <h3>Quick Links</h3>
+            <ul className='list-disc pl-7 flex flex-col gap-3'>
+              <li><a href="#">Home</a></li>
+
+
+              <li><a href="#">Services</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">About</a></li>
+          
+            </ul>
+          </div>
+          <div className="sub-container">
+            <h3>Subscribe</h3>
+            <p>Subscribe to our newsletter for the latest updates.</p>
+            <div className="sub flex items-center my-5">
+              <input type="text" className='h-10 appearance-none leading-2.5 bg-amber-50 rounded-r-none rounded-xl w-2/3 text-xs  px-4  py-2  text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Enter your email'/>
+              <a href="#" className=' text-gray-950 leading-6 text-center  w-1/3 bg-slate-300 px-3 py-2 font-bold rounded-l-none rounded-xl h-10 bg-linear-65 from-pink-300 to-yellow-400'>Subscribe</a>
+            
+            </div>
+          </div>
+        </div>
+        <div className="copy text-center">
+          <hr />
+          <p className=' py-10'>&copy;2024 Your Company All Rights Reserved.</p>
         </div>
       </div>
     </>
